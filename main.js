4a8de87b0659dbc2921ce533f0206eb7a9f48c3d
ysy <1838940294@qq.com>
2022-04-03 14:04:10 +0800

    feat: 1 - emmc compiler libwebp

---
+++ main.js
@@ -0,0 +1,13 @@
+async function loadImage(src) {
+  // Load image
+  const imgBlob = await fetch(src).then((resp) => resp.blob());
+  const img = await createImageBitmap(imgBlob);
+  // Make canvas same size as image
+  const canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  // Draw image onto canvas
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  return ctx.getImageData(0, 0, img.width, img.height);
+}
